4b6e952ff906efa3e42f0c1563582229f2646ab7
Dhanush CK <dhanushck01@gmail.com>
2025-05-14 23:53:14 +0530

    feat : Added new function

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from . forms import RegistrationForm
 
 # Create your views here.
+
+def home(request):
+    return render(request, "index.html", {'form_page': form_page})
+
+def form_page(request):
+    form = RegistrationForm()
+    return render(request, "form.html", {'form': form})
