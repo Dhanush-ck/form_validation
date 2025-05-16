6c1e6c0ce1c41886b2a2258d2b577b3fe174a275
Dhanush CK <dhanushck01@gmail.com>
2025-05-16 21:01:24 +0530

    feat : Created new view for result page

--- myapp/views.py
+++ myapp/views.py
@@ -6,6 +6,24 @@ from . forms import RegistrationForm
 def home(request):
     return render(request, "index.html", {'form_page': form_page})
 
+
 def form_page(request):
-    form = RegistrationForm()
-    return render(request, "form.html", {'form': form})
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            return render(request, 'result.html', {
+                'username': username,
+                'email': email,
+                'password': password,
+                })
+    else:
+        form = RegistrationForm()
+    return render(request, "form.html", {
+        'form': form,
+        })
+
+def result_page(request):
+    return render(request, "result.html")
