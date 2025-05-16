75374d76b64c8bcae2d5112a0ff2fcb19ae879ff
Dhanush CK <dhanushck01@gmail.com>
2025-05-16 21:02:01 +0530

    feat : Added new path for result page

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('form/', views.form_page, name='form')
+    path('form/', views.form_page, name='form'),
+    path('result/', views.result_page, name='result'),
 ]
